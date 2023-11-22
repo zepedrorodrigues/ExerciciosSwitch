15a48f5427af50a90ae88d7c847fecd2e8e41e6a
ozepedrorodrigues <1231837@isep.ipp.pt>
2023-11-22 22:09:01 +0000

    .

--- Bloco5/src/main/java/Array.java
+++ Bloco5/src/main/java/Array.java
@@ -1,61 +1,203 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Array {
-
     private int[] array1;
     private int size;
     private static final String error = "Invalid Argument";
-
-    public Array(){
+    public Array() {
         array1 = new int[0];}
-    public Array(int[] array)throws InstantiationException {
-        if(!validConstructorArgument(array))
+
+    public Array(int[] array) throws InstantiationException {
+        if (!validConstructorArgument(array))
             throw new InstantiationException(error);
-        this.size= array.length;
-    }
-    private boolean validConstructorArgument(int[] array1)
-        {return array1 != null;}
-    public int[] addElement(int x){
+        this.size = array.length;
+        this.array1 = array;}
+
+    private boolean validConstructorArgument(int[] array1) {
+        return array1 != null;}
+
+    public int[] addElement(int x) {
         array1 = Arrays.copyOf(array1, array1.length + 1);
-        array1[array1.length-1] = x;
+        array1[array1.length - 1] = x;
         return array1;}
-    public int[] removeElement(int x){
-        if(detectElementwithValue(x)==-1)
+
+    public int[] removeElement(int x) {
+        if (detectElementwithValue(x) == -1)
             return array1;
-        int[] newArray = new int[array1.length-1];
-        for(int i=0; i<array1.length; i++){
-            if(array1[i] == x)
+        int[] newArray = new int[array1.length - 1];
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] == x)
                 continue;
-            newArray[i] = array1[i];}
+            newArray[i] = array1[i];
+        }
         array1 = newArray;
         return array1;}
-    private int detectElementwithValue(int x){
+
+    private int detectElementwithValue(int x) {
         int contador = 0;
-        for(int value: array1){
-            if(value == x)
+        for (int value : array1) {
+            if (value == x)
                 return contador;
             contador++;}
         return -1;}
-    public int getMenorElemento()throws InstantiationException{
-        if(array1.length == 0)
+
+    public int getMenorElemento() throws InstantiationException {
+        if (array1.length == 0)
             throw new InstantiationException(error);
         int menor = array1[0];
-        for(int value: array1)
-            menor=Math.min(value, menor);
+        for (int value : array1)
+            menor = Math.min(value, menor);
         return menor;}
-    public int getMaiorElemento()throws InstantiationException{
-        if(array1.length == 0)
-            throw(new InstantiationException(error));
+
+    public int getMaiorElemento() throws InstantiationException {
+        if (array1.length == 0)
+            throw (new InstantiationException(error));
         int maior = array1[0];
-        for(int value: array1)
-            maior=Math.max(value, maior);
+        for (int value : array1)
+            maior = Math.max(value, maior);
         return maior;}
-    public int getSize(){
+
+    public int getSize() {
         return size;}
-    public int getElementbyIndex(int index){
-        if(index < 0 || index >= array1.length)
+
+    public int getElementbyIndex(int index) {
+        if (index < 0 || index >= array1.length)
             return -1;
         return array1[index];}
 
+    public int counterMultiplesofNumber(int mult, boolean yesorNo) throws InstantiationException{
+        if(mult == 0)
+            throw new InstantiationException(error);
+        int counter = 0;
+        if(yesorNo)
+            for (int value : array1) {
+                if (value % mult == 0)
+                    counter++;}
+        if(!yesorNo)
+            for (int value : array1) {
+                if (value % mult != 0)
+                    counter++;}
+        return counter;}
 
-}
+    public int averageArray(int[] array) throws InstantiationException{
+        if(array.length == 0)
+            throw new InstantiationException(error);
+        return sumOfArray(array) / array.length;}
+    public int sumOfArray(int[] array) {
+        int sum = 0;
+        for (int value : array)
+            sum += value;
+        return sum;}
+
+    public int[] getmultiplesofNumber(int mult, boolean multiOrNot) throws InstantiationException {
+        if (mult == 0)
+            throw new InstantiationException(error);
+        int[] multiplesArray = new int[counterMultiplesofNumber(mult,true)];
+        int counter = 0;
+        if(multiOrNot){
+            for (int value : array1) {
+                if (value % mult == 0) {
+                    multiplesArray[counter] = value;
+                    counter++;}}}
+        else{
+            multiplesArray = new int[counterMultiplesofNumber(mult, false)];
+            for (int value : array1) {
+                if (value % mult != 0) {
+                    multiplesArray[counter] = value;
+                    counter++;}}}
+        return multiplesArray;}
+
+    public int[] getSortedArray(boolean ascending){
+        if(ascending){
+            Arrays.sort(array1);
+            return array1;}
+        if (!ascending){
+            Integer[] array2 = new Integer[array1.length];
+            for(int i = 0; i < array1.length; i++)
+                array2[i] = array1[i];
+            Arrays.sort(array2, Collections.reverseOrder());
+            for(int i = 0; i < array1.length; i++)
+                array1[i] = array2[i];}
+        return array1;}
+    public boolean hasXElements(int x)throws InstantiationException{
+        if(x<0)
+            throw new InstantiationException(error);
+        return size == x;}
+    public boolean isAllEvensorOdds(boolean evens) throws InstantiationException{
+        if(array1.length == 0)
+            throw new InstantiationException(error);
+        if(!evens)
+            return (getmultiplesofNumber(2,false).length==array1.length);
+        return (getmultiplesofNumber(2,true).length==array1.length);}
+    public boolean allUniqueValues(){
+        for(int i = 0; i < array1.length; i++){
+            for(int j = i + 1; j < array1.length; j++){
+                if(array1[i] == array1[j])
+                    return true;}}
+        return false;}
+    public int howManyDigits(int testNumber){
+        int contador=0;
+        while(testNumber != 0){
+            testNumber /= 10;
+            contador++;}
+        return contador;}
+    public int[] arrayofNumberstoArrayofDigits(){
+        int[] array2 = new int[array1.length];
+        for(int i = 0; i < array1.length; i++){
+            array2[i] = howManyDigits(array1[i]);}
+        return array2;}
+    public double averageDigits(){
+        int[] arrayofDigits = arrayofNumberstoArrayofDigits();
+        return (double)sumOfArray(arrayofDigits) / arrayofDigits.length;}
+    public int counterElementsMoreDigitsThanAverage(){
+        int counter = 0;
+        for(int value: array1){
+            if(howManyDigits(value) > averageDigits())
+                counter++;}
+        return counter;}
+
+    public int[] getElementsWithMoreDigitsThanAverage() throws InstantiationException{
+        if(array1.length == 0)
+            throw new InstantiationException(error);
+        double averageDigits = averageDigits();
+        int[] array2 = new int[counterElementsMoreDigitsThanAverage()];
+        int counter = 0;
+        for(int value: array1){
+            if(howManyDigits(value) > averageDigits){
+                array2[counter] = value;
+                counter++;}}
+        return array2;}
+    public double percentageofEvenAlgarisms(int numberTest){
+        int counter = 0;
+        int numberofDigits = howManyDigits(numberTest);
+        while(numberTest != 0){
+            if(numberTest % 2 == 0)
+                counter++;
+            numberTest /= 10;}
+        return counter * 100 / (double)numberofDigits;}
+
+    public double averagePercentageofEvenAlgarisms(){
+        double sum = 0;
+        for(int value: array1){
+            sum += percentageofEvenAlgarisms(value);}
+        return sum / array1.length;}
+    public int counterElementsMoreEvenAlgarismsThanTheAverage(){
+        int counter = 0;
+        for(int value: array1){
+            if(percentageofEvenAlgarisms(value) > averagePercentageofEvenAlgarisms())
+                counter++;}
+        return counter;}
+
+    public int[] getElementsMoreEvenAlgarismsThanAverage() throws InstantiationException{
+        if(array1.length == 0)
+            throw new InstantiationException(error);
+        int[] array2 = new int[counterElementsMoreEvenAlgarismsThanTheAverage()];
+        int counter = 0;
+        for(int value: array1){
+            if(percentageofEvenAlgarisms(value) > averagePercentageofEvenAlgarisms()){
+                array2[counter] = value;
+                counter++;}}
+        return array2;}
+
+    }
--- Bloco5/src/test/java/ArrayTest.java
+++ Bloco5/src/test/java/ArrayTest.java
@@ -7,32 +7,41 @@ class ArrayTest {
         int[] array = {1, 2, 3};
         //Act
         Array a = new Array(array);
-        /*Assert não necessário, só desejamos ver se dá erro*/}
+        /*Assert não necessário, só desejamos ver se dá erro*/
+    }
 
     @Test
     void arrayInvalidConstructorArgument() throws InstantiationException {
         //Arrange and Act
         int[] array = null;
         //Assert
-        assertThrows(InstantiationException.class, () -> {new Array(array);});}
+        assertThrows(InstantiationException.class, () -> {
+            new Array(array);
+        });
+    }
 
     @Test
     void arrayVazioConstrutorSemParametros() throws InstantiationException {
         //Arrange, Act and Assert - tudo o mesmo porque chamamos construtor sem parâmetros
-        Array a = new Array();}
+        Array a = new Array();
+    }
 
     @Test
     void arrayVazionoConstrutorcomParametro() throws InstantiationException {
         int[] array = {};
-        Array a = new Array(array);}
+        Array a = new Array(array);
+    }
 
     @Test
     void getArrayWithValueXArrayNull() throws InstantiationException {
         //Arrange
         int[] array = null;
         //Assert
-        assertThrows(InstantiationException.class, () ->{new Array(array);});}
-    
+        assertThrows(InstantiationException.class, () -> {
+            new Array(array);
+        });
+    }
+
     @Test
     void getMenorElementoValid() throws InstantiationException {
         //Arrange
@@ -41,36 +50,46 @@ class ArrayTest {
         //Act
         int result = a.getMenorElemento();
         //Assert
-        assertEquals(1, result);}
-    
+        assertEquals(1, result);
+    }
+
     @Test
     void getMenorElementoInvalid() throws InstantiationException {
         //Arrange
         int[] array = {};
         Array newArray = new Array(array);
         //Act
-        Exception exception = assertThrows(InstantiationException.class,()->{newArray.getMenorElemento();});
+        Exception exception = assertThrows(InstantiationException.class, () -> {
+            newArray.getMenorElemento();
+        });
         //Assert
         String expectedMessage = "Invalid Argument";
-        assertEquals(expectedMessage, exception.getMessage());}
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+
     @Test
     void addElementValido() throws InstantiationException {
         //Arrange
         int[] array = {1, 2, 3};
         Array a = new Array(array);
         //Act
-        int[] expected = {1,2,3,4};
+        int[] expected = {1, 2, 3, 4};
         int[] result = a.addElement(4);
         //Assert
-        assertArrayEquals(expected,result);}
+        assertArrayEquals(expected, result);
+    }
+
     @Test
-    void addElementInvalido()throws InstantiationException {
+    void addElementInvalido() throws InstantiationException {
         //Arrange
         int[] array = null;
         //Assert
-        Exception exception = assertThrows(InstantiationException.class,()->
-        {new Array(array);});
-        assertEquals("Invalid Argument", exception.getMessage());}
+        Exception exception = assertThrows(InstantiationException.class, () ->
+        {
+            new Array(array);
+        });
+        assertEquals("Invalid Argument", exception.getMessage());
+    }
 
     @Test
     void getMaiorElementoValido() throws InstantiationException {
@@ -80,17 +99,22 @@ class ArrayTest {
         //Act
         int result = a.getMaiorElemento();
         //Assert
-        assertEquals(3, result);}
+        assertEquals(3, result);
+    }
+
     @Test
     void getMaiorElementoInvalido() throws InstantiationException {
         //Arrange
         int[] array = {};
         Array newArray = new Array(array);
         //Act
-        Exception exception = assertThrows(InstantiationException.class,()->{newArray.getMaiorElemento();});
+        Exception exception = assertThrows(InstantiationException.class, () -> {
+            newArray.getMaiorElemento();
+        });
         //Assert
         String expectedMessage = "Invalid Argument";
-        assertEquals(expectedMessage, exception.getMessage());}
+        assertEquals(expectedMessage, exception.getMessage());
+    }
 
     @Test
     void getSizeValido() throws InstantiationException {
@@ -100,7 +124,9 @@ class ArrayTest {
         //Act
         int result = a.getSize();
         //Assert
-        assertEquals(3, result);}
+        assertEquals(3, result);
+    }
+
     @Test
     void getSizeArrayVazio() throws InstantiationException {
         //Arrange
@@ -109,61 +135,416 @@ class ArrayTest {
         //Act
         int result = a.getSize();
         //Assert
-        assertEquals(0, result);}
+        assertEquals(0, result);
+    }
+
     @Test
-    void removeElementValid()throws InstantiationException{
+    void removeElementValid() throws InstantiationException {
         //Arrange
         int[] array = {1, 2, 3};
         Array a = new Array(array);
         //Act
-        int[] expected = {1,2};
+        int[] expected = {1, 2};
         int[] result = a.removeElement(3);
         //Assert
-        assertArrayEquals(expected,result);}
+        assertArrayEquals(expected, result);
+    }
+
     @Test
-    void removeElementInvalid()throws InstantiationException{
+    void removeElementInvalid() throws InstantiationException {
         //Arrange
         int[] array = {1, 2, 3};
         Array a = new Array(array);
         //Act
-        int[] expected = {1,2,3};
+        int[] expected = {1, 2, 3};
         int[] result = a.removeElement(4);
         //Assert
-        assertArrayEquals(expected,result);}
+        assertArrayEquals(expected, result);
+    }
+
     @Test
-    void getElementbyIndexValid()throws InstantiationException{
+    void getElementbyIndexValid() throws InstantiationException {
         //Arrange
         int[] array = {1, 2, 3};
         Array a = new Array(array);
         //Act
         int result = a.getElementbyIndex(2);
         //Assert
-        assertEquals(3,result);}
+        assertEquals(3, result);
+    }
+
     @Test
-    void getElementbyIndexInvalid()throws InstantiationException{
+    void getElementbyIndexInvalid() throws InstantiationException {
         //Arrange
         int[] array = {1, 2, 3};
         Array a = new Array(array);
         //Act
         int result = a.getElementbyIndex(3);
         //Assert
-        assertEquals(-1,result);}
+        assertEquals(-1, result);
+    }
+
     @Test
-    void getElementbyIndex0()throws InstantiationException{
+    void counterMultiplesofNumberValid() throws InstantiationException {
         //Arrange
         int[] array = {1, 2, 3};
         Array a = new Array(array);
         //Act
-        int result = a.getElementbyIndex(0);
+        int result = a.counterMultiplesofNumber(2, true);
         //Assert
-        assertEquals(1,result);}
+        assertEquals(1, result);
+    }
+
     @Test
-    void getElementbyIndexNegative()throws InstantiationException{
+    void counterMultiplesofNumberNoMultiples() throws InstantiationException {
         //Arrange
         int[] array = {1, 2, 3};
         Array a = new Array(array);
         //Act
-        int result = a.getElementbyIndex(-1);
+        int result = a.counterMultiplesofNumber(4, true);
         //Assert
-        assertEquals(-1,result);}
+        assertEquals(0, result);
+    }
+
+    @Test
+    void counterMultiplesofNumberInvalid() throws InstantiationException {
+        //Arrange
+        int[] array = {1, 2, 3};
+        Array a = new Array(array);
+        //Assert
+        Exception exception = assertThrows(InstantiationException.class, () ->
+        {
+            a.counterMultiplesofNumber(0, true);
+        });
+        assertEquals("Invalid Argument", exception.getMessage());
+    }
+
+    @Test
+    void counterMultiplesofNumber1() throws InstantiationException {
+        //Arrange
+        int[] array = {1, 2, 3};
+        Array a = new Array(array);
+        //Act
+        int result = a.counterMultiplesofNumber(1, true);
+        //Assert
+        assertEquals(3, result);
+    }
+
+    @Test
+    void counterNotMultiplesofNumber2() throws InstantiationException {
+        //Arrange
+        int[] array = {1, 2, 3, 4, 5, 6};
+        Array a = new Array(array);
+        //Act
+        int result = a.counterMultiplesofNumber(2, false);
+        //Assert
+        assertEquals(3, result);
+    }
+
+    @Test
+    void counterNotMultiplesofNumber3NoMultiple() throws InstantiationException {
+        //Arrange
+        int[] array = {9, 0, 3, 9, 6, 6};
+        Array a = new Array(array);
+        //Act
+        int result = a.counterMultiplesofNumber(3, false);
+        //Assert
+        assertEquals(0, result);
+    }
+
+    @Test
+    void counterNotMultiplesofNumber0() throws InstantiationException {
+        //Arrange
+        int[] array = {1, 2, 3, 4, 5, 6};
+        Array a = new Array(array);
+        //Assert
+        Exception exception = assertThrows(InstantiationException.class, () ->
+        {
+            a.counterMultiplesofNumber(0, false);
+        });
+        assertEquals("Invalid Argument", exception.getMessage());
+    }
+
+    @Test
+    void counterNotMultiplesofNumber1AllAreMultiples() throws InstantiationException {
+        //Arrange
+        int[] array = {1, 2, 3, 4, 5, 6};
+        Array a = new Array(array);
+        //Act
+        int result = a.counterMultiplesofNumber(1, false);
+        //Assert
+        assertEquals(0, result);
+    }
+
+    @Test
+    void avertageArrayValid() throws InstantiationException {
+        //Arrange
+        int[] array = {1, 2, 3, 4, 5, 6};
+        Array a = new Array(array);
+        //Act
+        int result = a.averageArray(array);
+        //Assert
+        assertEquals(3, result);
+    }
+
+    @Test
+    void averageArrayInvalid() throws InstantiationException {
+        //Arrange
+        int[] array = {};
+        Array a = new Array(array);
+        //Assert
+        Exception exception = assertThrows(InstantiationException.class, () ->
+        {
+            a.averageArray(array);
+        });
+        assertEquals("Invalid Argument", exception.getMessage());
+    }
+
+    @Test
+    void averageArrayOneDigit() throws InstantiationException {
+        //Arrange
+        int[] array = {1};
+        Array a = new Array(array);
+        //Act
+        int result = a.averageArray(array);
+        //Assert
+        assertEquals(1, result);
+    }
+
+    @Test
+    void averagerrayAllZeros() throws InstantiationException {
+        //Arrange
+        int[] array = {0, 0, 0, 0, 0, 0};
+        Array a = new Array(array);
+        //Act
+        int result = a.averageArray(array);
+        //Assert
+        assertEquals(0, result);
+    }
+
+    @Test
+    void sumofArrayValid() throws InstantiationException {
+        //Arrange
+        int[] array = {1, 2, 3, 4, 5, 6};
+        Array a = new Array(array);
+        //Act
+        int result = a.sumOfArray(array);
+        //Assert
+        assertEquals(21, result);
+    }
+
+    @Test
+    void getmultiplesofNummbervalid() throws InstantiationException {
+        //Arrange
+        int[] array = {1, 2, 3, 4, 5, 6};
+        int[] expected = {2, 4, 6};
+        Array a = new Array(array);
+        //Act
+        int[] result = a.getmultiplesofNumber(2, true);
+        //Assert
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void getmultiplesofNumberZero() throws InstantiationException {
+        //Arrange
+        int[] array = {1, 2, 3, 4, 5, 6};
+        Array a = new Array(array);
+        //Assert
+        Exception exception = assertThrows(InstantiationException.class, () ->
+        {
+            a.getmultiplesofNumber(0, true);
+        });
+        assertEquals("Invalid Argument", exception.getMessage());
+    }
+
+    @Test
+    void getMultiplesofNumber1AllNumbers() throws InstantiationException {
+        //Arrange
+        int[] array = {1, 2, 3, 4, 5, 6};
+        int[] expected = {1, 2, 3, 4, 5, 6};
+        Array a = new Array(array);
+        //Act
+        int[] result = a.getmultiplesofNumber(1, true);
+        //Assert
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void getMultiplesofNumber3NoMultiples() throws InstantiationException {
+        //Arrange
+        int[] array = {7, 2, 8, 4, 5, 11};
+        int[] expected = {};
+        Array a = new Array(array);
+        //Act
+        int[] result = a.getmultiplesofNumber(3, true);
+        //Assert
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void getNotMultiplesof3() throws InstantiationException {
+        //Arrange
+        int[] array = {7, 2, 8, 4, 5, 11};
+        int[] expected = {7, 2, 8, 4, 5, 11};
+        Array a = new Array(array);
+        //Act
+        int[] result = a.getmultiplesofNumber(3, false);
+        //Assert
+        assertArrayEquals(expected, result);
+    }
+    @Test
+    void getNotMultiplesofNumber0() throws InstantiationException {
+        //Arrange
+        int[] array = {7, 2, 8, 4, 5, 11};
+        Array a = new Array(array);
+        //Assert
+        Exception exception = assertThrows(InstantiationException.class, () ->
+        {
+            a.getmultiplesofNumber(0, false);
+        });
+        assertEquals("Invalid Argument", exception.getMessage());
+    }
+    @Test
+    void getNotMultiplesof1AllNumbers() throws InstantiationException {
+        //Arrange
+        int[] array = {7, 2, 8, 4, 5, 11};
+        int[] expected = {};
+        Array a = new Array(array);
+        //Act
+        int[] result = a.getmultiplesofNumber(1, false);
+        //Assert
+        assertArrayEquals(expected, result);
+    }
+    @Test
+    void getSortedArrayValid() throws InstantiationException {
+        //Arrange
+        int[] array = {7, 2, 8, 4, 5, 11};
+        int[] expected = {2, 4, 5, 7, 8, 11};
+        Array a = new Array(array);
+        //Act
+        int[] result = a.getSortedArray(true);
+        //Assert
+        assertArrayEquals(expected, result);
+    }
+    @Test
+    void getSortedArrayVazio() throws InstantiationException {
+        //Arrange
+        int[] array = {};
+        int[] expected = {};
+        Array a = new Array(array);
+        //Act
+        int[] result = a.getSortedArray(true);
+        //Assert
+        assertArrayEquals(expected, result);
+    }
+    @Test
+    void getSortedArrayDescendente() throws InstantiationException {
+        //Arrange
+        int[] array = {7, 2, 8, 4, 5, 11};
+        int[] expected = {11, 8, 7, 5, 4, 2};
+        Array a = new Array(array);
+        //Act
+        int[] result = a.getSortedArray(false);
+        //Assert
+        assertArrayEquals(expected, result);
+    }
+    @Test
+    void getSortedArrayAscendenteTodosIguais() throws InstantiationException {
+        //Arrange
+        int[] array = {1, 1, 1, 1, 1, 1};
+        int[] expected = {1, 1, 1, 1, 1, 1};
+        Array a = new Array(array);
+        //Act
+        int[] result = a.getSortedArray(true);
+        //Assert
+        assertArrayEquals(expected, result);
+    }
+    @Test
+    void hasXElementsValid() throws InstantiationException {
+        //Arrange
+        int[] array = {1, 1, 1, 1, 1, 1};
+        Array a = new Array(array);
+        //Act
+        boolean result = a.hasXElements(6);
+        //Assert
+        assertTrue(result);}
+    @Test
+    void hasXElementsZeroElements() throws InstantiationException {
+        //Arrange
+        int[] array = {};
+        Array a = new Array(array);
+        //Act
+        boolean result = a.hasXElements(0);
+        //Assert
+        assertTrue(result);}
+    @Test
+    void hasXElementsInvalid() throws InstantiationException {
+        //Arrange
+        int[] array = {1, 1, 1, 1, 1, 1};
+        Array a = new Array(array);
+        //Act
+        boolean result = a.hasXElements(7);
+        //Assert
+        assertFalse(result);}
+    @Test
+    void hasXElementsXequalsNegative()throws InstantiationException{
+        //Arrange
+        int[] array = {1, 1, 1, 1, 1, 1};
+        Array a = new Array(array);
+        //Assert
+        Exception exception = assertThrows(InstantiationException.class, () ->
+        {
+            a.hasXElements(-1);
+        });
+        assertEquals("Invalid Argument", exception.getMessage());}
+    @Test
+    void isAllEvensorOddsValid() throws InstantiationException {
+        //Arrange
+        int[] array = {2, 4, 6, 8, 10, 12};
+        Array a = new Array(array);
+        //Act
+        boolean result = a.isAllEvensorOdds(true);
+        //Assert
+        assertTrue(result);}
+    @Test
+    void isAllEvensorOddsInvalid() throws InstantiationException {
+        //Arrange
+        int[] array = {};
+        Array a = new Array(array);
+        //Assert
+        Exception exception = assertThrows(InstantiationException.class, () ->
+        {
+            a.isAllEvensorOdds(true);
+        });
+        assertEquals("Invalid Argument", exception.getMessage());}
+    @Test
+    void isAllEvensorOddsAllOdds() throws InstantiationException {
+        //Arrange
+        int[] array = {1, 3, 5, 7, 9, 11};
+        Array a = new Array(array);
+        //Act
+        boolean result = a.isAllEvensorOdds(false);
+        //Assert
+        assertTrue(result);}
+    @Test
+    void isAllEvensorOdds() throws InstantiationException {
+        //Arrange
+        int[] array = {2, 4,6, 8, 10, 12};
+        Array a = new Array(array);
+        //Act
+        boolean result = a.isAllEvensorOdds(false);
+        //Assert
+        assertFalse(result);}
+    @Test
+    void isAllOddsSomeAre() throws InstantiationException {
+        //Arrange
+        int[] array = {1, 2, 3, 4, 5, 6};
+        Array a = new Array(array);
+        //Act
+        boolean result = a.isAllEvensorOdds(false);
+        //Assert
+        assertFalse(result);}
+
+
     }
